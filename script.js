7206aac1fe4e453a05dbcc3d415dffa608fe2614
DigitalDesignDude <106634287+DigitalDesignDude@users.noreply.github.com>
2022-09-17 04:57:25 -0300

    Added Coding Reference Page

--- script.js
+++ script.js
@@ -29,11 +29,14 @@ $(document).ready(function () {
 
             // Store hash
             var hash = this.hash;
+            
+            //Store extra offset value
+            var offsetY = 60
 
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top - offsetY //
             }, 800, function () {
 
                 // Add hash (#) to URL when done scrolling (default click behavior)
